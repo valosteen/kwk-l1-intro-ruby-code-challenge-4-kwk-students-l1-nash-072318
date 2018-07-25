a1d62d1a19e3b520674f12b7952142cfba761629
valosteen <val_osteen@oneidaschool.org>
2018-07-25 19:00:11 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -5,10 +5,20 @@ solution ={}
 
 num_quarters = cents / 25 
 solution[:quarters]= num_quarters 
+cents = cents - 25*num_quarters
+
 
 num_dimes = cents/10 
 solution[:dimes]= num_dimes
+cents = cents - 10*num_dimes
 
+num_nickles = cents/5
+solution [:nickles]= num_nickles
+cents = cents - 5*num_nickles
+
+num_pennies = cents/1 
+solution [:pennies]= num_pennies
+cents = cents - 1*num_pennies
 
 puts solution
 
