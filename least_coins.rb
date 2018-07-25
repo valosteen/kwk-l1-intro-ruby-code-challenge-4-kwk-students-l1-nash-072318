65ec25e49f2449f85b68ab4f081b4aa073963960
valosteen <val_osteen@oneidaschool.org>
2018-07-25 18:32:27 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,17 @@
 #write out your code here
 
 def least_coins(cents)
+  coins={"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies"=> 0}
+  If "pennies"<5 
+  put "pennies"=1
+  
+elsif "nickels" = 5
+put "nickels"= 1
+  
+  
 
 #Code your answer here!
+
+
 
 end
