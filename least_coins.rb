ba5489abf1c9f422925b03a28b0a399dfda7c382
valosteen <val_osteen@oneidaschool.org>
2018-07-25 18:47:42 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,24 +1,10 @@
 #write out your code here
 
 def least_coins(cents)
-  coins={"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies"=> 0}
-  if "pennies"<5 
-  puts "pennies"=1
-  
-elsif "nickels" = 5
-puts "nickels"= 1
+solution ={}
 
-elsif "dimes" = 10
-puts "dimes" = 1 
-
-else "quarters" = 25
-  puts "quarters" = 1
-
-end 
-  
-  least_coins
-
-#Code your answer here!
+quarters = cents / 25 
+solution[:quarters]=
 
 
 
